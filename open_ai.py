6a3f8e5f9495655b299fa27dff20e4f26d931fa6
kimdonghyuk <kdh367036@naver.com>
2024-11-09 00:07:37 +0900

    FEAT : damage반환 추가

--- open_ai.py
+++ open_ai.py
@@ -17,7 +17,34 @@ client = OpenAI(api_key = api_key )
 class StoryRequest(BaseModel):
     choice: Optional[str] = None  # 선택적 필드로 설정
     beforeContent: Optional[str] = ""  # 이전 생성된 내용을 저장하는 선택적 필드, 기본값은 빈 문자열
+class ImageRequest(BaseModel):
+    description: Optional[str] = None #이미지 묘사 텍스트
 
+#이미지 생성
+@app.post("/generate_image")
+async def generate_story(request: ImageRequest):
+    desc = request.description
+
+    try:
+        response = client.images.generate(
+             model="dall-e-3",
+             prompt=desc,
+             size="1024x1024",
+             quality="hd",
+             n=1,
+            )
+        # 이미지 URL 추출
+        image_url = response.data[0].url
+
+         # JSON 형태로 변환
+        return {"image_url": image_url}
+    
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        raise HTTPException(status_code=500, detail=f"Error generating image: {e}")
+
+
+# 스토리 생성
 @app.post("/generate_story")
 async def generate_story(request: StoryRequest):
     # choice 값이 없으면 기본값으로 "게임을 시작해줘" 사용
@@ -46,14 +73,17 @@ async def generate_story(request: StoryRequest):
             "말은 항상 한글로 해."
             "현재 상황을 설명할때는 최대한 자세하게 설명해줘 3줄이상"
             "맹수를 만나기, 절벽을 마주치기, 넘어져서 상처를 입기, 기상악화 등등 여러 극적인 상황들도 생성해줘"
-            "선택지에 없는 행동을 하면 다시 올바르게 선택하라고 알려줘."
+            "'선택지에 없는 행동을 하면 다시 올바르게 선택하라고 알려줘."
+            "'물을 먹는다.', '음식을 먹는다.' 같은 음식과 물을 소비하는 경우는 없어. 물과 음식은 플레이어가 직접 선택하여 먹을 수 있게 따로 관리하고 있어."
             "반환 형식은 JSON 형식으로 해야 하고, 각 필드는 다음과 같아:"
             "사용자의 질문이 '게임을 시작해줘'일 경우에 water,food의 변화량은 무조건 0이야"
             "- `content`: 현재 상황 설명"
             "- `choice1`, `choice2`, `choice3`: 플레이어가 선택할 수 있는 세 가지 선택지 텍스트"
-            "- `water`: 각 선택이 물에 미치는 변화량 (예: -3,-2,-1, 1, 2, 3)"
-            "- `food`: 각 선택이 음식에 미치는 변화량 (예: -3,-2,-1, 1, 2, 3)"
+            "- `water`: 현재 선택이 물에 미치는 변화량 (변화량 범위는: -1, 0, 1, 2, 3)"
+            "- `food`: 현재 선택이 음식에 미치는 변화량 (변화량 범위는 : -1, 0, 1, 2, 3)"
+            "- `demage`: 현재 선택이 플레이어에게 입히는 피해(피해량 범위는 : 0 ~ 5)"
             "- `description`: 텍스트만보고 그림을 그릴 수 있게 현재 상황의 한 순간을 글로 표현,주어(주어는 무조건 남자로 한다.) 목적어 서술어가 무조건 있어야 한다. (예: 우주복을 입고 아마존의 풀숲에서 앉아서 휴식을 취하고 있는 한 남자. 뒤에는 남자가 탔던 우주선이 있다.) 예시를 똑같이 쓰지마"
+            "상황에 맞게 water,food,demage의 변화량을 잘 설정해줘."
         )
     },
     {
